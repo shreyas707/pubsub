b44a0d73fdf8bab6744b977fc0052b0ba8ddf84c
Shreyas S <hello@shreyas.im>
2019-12-31 17:14:10 +0530

    wrote issues in App.js

--- subscriber/src/App.js
+++ subscriber/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import redis from 'redis'
 
+// importing only redis throws "Module not found: Can't resolve hiredis" error
+// https://stackoverflow.com/questions/51018467/trying-to-access-redis-via-react-cant-resolve-hiredis
+
+// doing npm install hiredis throws this error:
+// https://github.com/redis/hiredis-node/issues/143
+
 class App extends React.Component {
   constructor(props){
     super(props)
-    
+
+    const client = redis.createClient();
+    client.subscribe("publisher1")
+
     this.state = {
       msg: ""
     }
