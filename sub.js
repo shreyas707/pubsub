438848eb9ae12609aaca251e5aecc50033c4ba37
Shreyas S <hello@shreyas.im>
2020-01-01 23:23:15 +0530

    notification works

--- sub.js
+++ sub.js
@@ -1,22 +1,38 @@
 const express = require('express')
 const redis = require('redis')
+const socket = require('socket.io')
 
 const subscriber = redis.createClient();
 
 const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+// running server
+const server = app.listen(port, function () {
+    console.log('listening to port', port)
+})
+
+// connect to sockets
+const io = socket(server)
+
+io.on('connection', function (socket) {
+    console.log('made socket connection')
+})
+
+// subscribing to a channel
+const channel = "publisher1"
+
+subscriber.subscribe(channel, function () {
+    console.log('subscribed to', channel)
+})
 
 subscriber.on(
-    'message', 
-    (channel, message) => { 
-        console.log(`data: ${message}`) 
+    'message',
+    (channel, message) => {
+
+        // Handle notification event
+        io.emit('notification', JSON.parse(message))
+
     })
-
-subscriber.subscribe("publisher1")
-
-app.get('/', (req, res) => {
-    res.send('Subscriber 1')
-})
-
-app.listen(3006,() => {
-    console.log("server is listening to port 3006");
-})
